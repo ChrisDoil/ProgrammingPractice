07e2decfdf97e475281718ae22640912be24874c
root <root@localhost.localdomain>
2019-11-01 17:40:28 -0400

    201911011640

---
+++ array_reversal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int num, *arr, i;
+    int temp = 0;
+
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    printf("%d\n", arr[0]);
+
+    /* Reverse */
+    for(i = 0; i < (num / 2); i++) {
+        temp = arr[i];
+        arr[i] = arr[num - i - 1];
+        arr[num - i - 1] = temp;
+    }
+
+
+    for(i = 0; i < num; i++) {
+        printf("%d ", *(arr + i));
+    
+    }
+    printf("\n");
+    return 0;
+}
